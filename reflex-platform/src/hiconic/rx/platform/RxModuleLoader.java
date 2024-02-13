ff803c5573fc785e856d9b9c757bcc28dcdb38dc
Peter Gazdik <peter.gazdik@gmail.com>
2024-02-13 15:37:51 +0100

    ModuleLoader minor cleanup

--- reflex-platform/src/hiconic/rx/platform/RxModuleLoader.java
+++ reflex-platform/src/hiconic/rx/platform/RxModuleLoader.java
@@ -1,7 +1,6 @@
 package hiconic.rx.platform;
 
 import static com.braintribe.console.ConsoleOutputs.brightBlack;
-import static com.braintribe.console.ConsoleOutputs.brightBlue;
 import static com.braintribe.console.ConsoleOutputs.cyan;
 import static com.braintribe.console.ConsoleOutputs.println;
 import static com.braintribe.console.ConsoleOutputs.sequence;
@@ -19,7 +18,6 @@ import java.util.Properties;
 
 import com.braintribe.cfg.LifecycleAware;
 import com.braintribe.cfg.Required;
-import com.braintribe.console.ConsoleOutputs;
 import com.braintribe.gm.model.reason.Maybe;
 import com.braintribe.gm.model.reason.Reasons;
 import com.braintribe.gm.model.reason.config.ConfigurationError;
@@ -27,8 +25,6 @@ import com.braintribe.gm.model.reason.essential.InternalError;
 import com.braintribe.gm.model.reason.essential.IoError;
 import com.braintribe.gm.model.reason.essential.NotFound;
 import com.braintribe.logging.Logger;
-import com.braintribe.model.processing.meta.configured.ConfigurationModelBuilder;
-import com.braintribe.model.processing.service.common.ConfigurableDispatchingServiceProcessor;
 import com.braintribe.utils.lcd.LazyInitialized;
 import com.braintribe.wire.api.Wire;
 import com.braintribe.wire.api.context.WireContext;
@@ -37,70 +33,72 @@ import com.braintribe.wire.api.module.WireTerminalModule;
 import hiconic.rx.module.api.wire.RxModuleContract;
 
 public class RxModuleLoader implements LifecycleAware {
+
 	private static final Logger logger = Logger.getLogger(RxModuleLoader.class);
+
 	private WireContext<?> parentContext;
-	private List<WireContext<? extends RxModuleContract>> contexts;
-	private ConfigurableDispatchingServiceProcessor dispatching;
-	private ConfigurationModelBuilder configurationModelBuilder;
-	
+	private List<WireContext<RxModuleContract>> contexts;
+
 	@Required
 	public void setParentContext(WireContext<?> parentContext) {
 		this.parentContext = parentContext;
 	}
-	
+
 	public Iterable<RxModuleContract> getModuleContracts() {
-		return () -> contexts.stream().map(c -> (RxModuleContract)c.contract()).iterator();
+		return () -> contexts.stream() //
+				.map(c -> c.contract()) //
+				.iterator();
 	}
-	
+
 	@Override
 	public void postConstruct() {
 		println("Loading Modules:");
 		loadModules();
 		println();
 	}
-	
+
 	@Override
 	public void preDestroy() {
 		closeWireModuleContexts();
 	}
-	
+
 	private void loadModules() {
-		Maybe<List<WireContext<? extends RxModuleContract>>> contextsMaybe = loadWireModules() //
-			.flatMap(this::loadWireModuleContexts);
-		
+		Maybe<List<WireContext<RxModuleContract>>> contextsMaybe = loadWireModules() //
+				.flatMap(this::loadWireModuleContexts);
+
 		contexts = contextsMaybe.get();
 	}
-	
+
 	private void closeWireModuleContexts() {
-		for (WireContext<? extends RxModuleContract> context: contexts) {
+		for (WireContext<RxModuleContract> context : contexts) {
 			context.close();
 		}
 	}
-	
-	public Maybe<List<WireTerminalModule<? extends RxModuleContract>>> loadWireModules() {
+
+	private Maybe<List<WireTerminalModule<RxModuleContract>>> loadWireModules() {
 		try {
 			Enumeration<URL> resources = getClass().getClassLoader().getResources("META-INF/rx-module.properties");
-			
+
 			LazyInitialized<ConfigurationError> lazyError = new LazyInitialized<>( //
 					() -> ConfigurationError.create("Error while loading rx-module configurations"));
-			
-			List<WireTerminalModule<? extends RxModuleContract>> wireModules = new ArrayList<>();
-			
+
+			List<WireTerminalModule<RxModuleContract>> wireModules = new ArrayList<>();
+
 			while (resources.hasMoreElements()) {
 				URL url = resources.nextElement();
-				Maybe<WireTerminalModule<? extends RxModuleContract>> wireModuleMaybe = loadWireModule(url);
-				
+				Maybe<WireTerminalModule<RxModuleContract>> wireModuleMaybe = loadWireModule(url);
+
 				if (wireModuleMaybe.isUnsatisfied())
 					lazyError.get().getReasons().add(wireModuleMaybe.whyUnsatisfied());
-				
+
 				wireModules.add(wireModuleMaybe.get());
 			}
-			
+
 			if (lazyError.isInitialized())
 				return lazyError.get().asMaybe();
-			
+
 			return Maybe.complete(wireModules);
-			
+
 		} catch (IOException e) {
 			return Reasons.build(IoError.T) //
 					.text("Could not enumerate classpath resources with the name 'META-INF/rx-module.properties'") //
@@ -108,123 +106,141 @@ public class RxModuleLoader implements LifecycleAware {
 					.toMaybe();
 		}
 	}
-	
-	public Maybe<WireTerminalModule<? extends RxModuleContract>> loadWireModule(URL propertiesUrl) {
+
+	public Maybe<WireTerminalModule<RxModuleContract>> loadWireModule(URL propertiesUrl) {
 		Properties properties = new Properties();
-		
+
 		try (Reader reader = new InputStreamReader(propertiesUrl.openStream(), "UTF-8")) {
 			properties.load(reader);
+
 		} catch (IOException e) {
 			return Reasons.build(IoError.T) //
 					.text("Could not read properties from " + propertiesUrl) //
 					.cause(InternalError.from(e)) //
 					.toMaybe();
 		}
-		
+
 		String wireModule = properties.getProperty("wire-module");
-		
-		
 		if (wireModule == null)
 			return Reasons.build(ConfigurationError.T).text("Missing wire-module property in " + propertiesUrl).toMaybe();
-		
-		
+
 		int index = wireModule.lastIndexOf('.');
-		
+
 		String pckg = wireModule.substring(0, index);
 		String name = wireModule.substring(index + 1);
-		
+
 		println( //
-				sequence(
-					text("  - "), //
-					cyan(name), //
-					brightBlack(" (" + pckg + ")") //
-				)
-		);
-		
+				sequence(text("  - "), //
+						cyan(name), //
+						brightBlack(" (" + pckg + ")") //
+				));
+
 		Class<?> wireModuleClass;
 		try {
 			wireModuleClass = Class.forName(wireModule);
+
 		} catch (ClassNotFoundException e) {
-			return Reasons.build(ConfigurationError.T).text("Could not find class " + wireModule +" configured with wire-module property in " + propertiesUrl).toMaybe();
+			return Reasons.build(ConfigurationError.T) //
+					.text("Could not find class " + wireModule + " configured with wire-module property in " + propertiesUrl) //
+					.toMaybe();
 		}
-		
+
 		if (!wireModuleClass.isEnum())
-			return Reasons.build(ConfigurationError.T).text("Class " + wireModule +" configured with wire-module property in " + propertiesUrl + " is not an enum class").toMaybe();
-		
-		var enumClass = (Class<? extends Enum>)wireModuleClass;
+			return Reasons.build(ConfigurationError.T) //
+					.text("Class " + wireModule + " configured with wire-module property in " + propertiesUrl + " is not an enum class") //
+					.toMaybe();
+
+		@SuppressWarnings("rawtypes")
+		var enumClass = (Class<? extends Enum>) wireModuleClass;
 
 		Enum<?> constant;
-		
+
 		try {
 			constant = Enum.valueOf(enumClass, "INSTANCE");
+
 		} catch (IllegalArgumentException e) {
-			return Reasons.build(ConfigurationError.T).text("Enum class " + wireModule +" configured with wire-module property in " + propertiesUrl + " is missing a constant INSTANCE").toMaybe();
+			return Reasons.build(ConfigurationError.T) //
+					.text("Enum class " + wireModule + " configured with wire-module property in " + propertiesUrl
+							+ " is missing a constant INSTANCE") //
+					.toMaybe();
 		}
-		
+
 		if (!(constant instanceof WireTerminalModule))
-			return Reasons.build(NotFound.T).text("Constant INSTANCE of enum class " + wireModule +" configured with wire-module property in " + propertiesUrl + " is not a WireTerminalModule").toMaybe();
-		
-		var wireTerminalModule = (WireTerminalModule<? extends RxModuleContract>) constant;
-		
+			return Reasons.build(NotFound.T) //
+					.text("Constant INSTANCE of enum class " + wireModule + " configured with wire-module property in " + propertiesUrl
+							+ " is not a WireTerminalModule") //
+					.toMaybe();
+
+		var wireTerminalModule = (WireTerminalModule<RxModuleContract>) constant;
+
 		if (!RxModuleContract.class.isAssignableFrom(wireTerminalModule.contract())) {
-			return Reasons.build(ConfigurationError.T).text("Constant INSTANCE of enum class " + wireModule +" configured with wire-module property in " + propertiesUrl + " is not a WireTerminalModule with a contract of type RxModuleContract").toMaybe();
+			return Reasons.build(ConfigurationError.T) //
+					.text("Constant INSTANCE of enum class " + wireModule + " configured with wire-module property in " + propertiesUrl
+							+ " is not a WireTerminalModule with a contract of type RxModuleContract") //
+					.toMaybe();
 		}
-		
+
 		return Maybe.complete(wireTerminalModule);
-			
 	}
-	
-	private Maybe<List<WireContext<? extends RxModuleContract>>> loadWireModuleContexts(List<WireTerminalModule<? extends RxModuleContract>> wireModules) {
-		List<WireContext<? extends RxModuleContract>> contexts = new ArrayList<>(wireModules.size());
-		
-		LazyInitialized<ConfigurationError> lazyError = new LazyInitialized<>(() -> ConfigurationError.create("Error while loading rx-module wire contexts"));
-		
-		for (WireTerminalModule<? extends RxModuleContract> wireModule: wireModules) {
-			Maybe<WireContext<? extends RxModuleContract>> contextMaybe = loadWireModuleContext(wireModule);
-			
+
+	private Maybe<List<WireContext<RxModuleContract>>> loadWireModuleContexts(List<WireTerminalModule<RxModuleContract>> wireModules) {
+
+		List<WireContext<RxModuleContract>> contexts = new ArrayList<>(wireModules.size());
+
+		LazyInitialized<ConfigurationError> lazyError = new LazyInitialized<>(
+				() -> ConfigurationError.create("Error while loading rx-module wire contexts"));
+
+		for (WireTerminalModule<RxModuleContract> wireModule : wireModules) {
+			Maybe<WireContext<RxModuleContract>> contextMaybe = loadWireModuleContext(wireModule);
+
 			if (contextMaybe.isUnsatisfied()) {
 				lazyError.get().getReasons().add(contextMaybe.whyUnsatisfied());
 			}
-			
+
 			contexts.add(contextMaybe.get());
 		}
-		
+
 		if (lazyError.isInitialized())
 			return lazyError.get().asMaybe();
-		
+
 		return Maybe.complete(contexts);
 	}
-	
-	private Maybe<WireContext<? extends RxModuleContract>> loadWireModuleContext(WireTerminalModule<? extends RxModuleContract> wireModule) {
+
+	private Maybe<WireContext<RxModuleContract>> loadWireModuleContext(WireTerminalModule<RxModuleContract> wireModule) {
 		try {
-			WireContext<? extends RxModuleContract> wireContext = Wire.contextBuilder(wireModule).parent(parentContext).build();
+			WireContext<RxModuleContract> wireContext = Wire.contextBuilder(wireModule).parent(parentContext).build();
 			return Maybe.complete(wireContext);
+
 		} catch (Exception e) {
 			logger.error("Error while loading module " + wireModule);
-			return Reasons.build(ConfigurationError.T).text("Could not load WireContext for WireModule " + wireModule.getClass().getName()) //
-					.cause(InternalError.from(e)).toMaybe();
+			return Reasons.build(ConfigurationError.T) //
+					.text("Could not load WireContext for WireModule " + wireModule.getClass().getName()) //
+					.cause(InternalError.from(e)) //
+					.toMaybe();
 		}
 	}
-	
+
 	public static Properties readApplicationProperties() {
 		Enumeration<URL> resources;
 		try {
 			resources = RxModuleLoader.class.getClassLoader().getResources("META-INF/rx-app.properties");
+
 		} catch (IOException e) {
 			throw new UncheckedIOException("Could not determine META-INF/rx-app.properties class path resources", e);
 		}
-		
+
 		Properties properties = new Properties();
 
 		while (resources.hasMoreElements()) {
 			URL url = resources.nextElement();
 			try (Reader reader = new InputStreamReader(url.openStream(), "UTF-8")) {
 				properties.load(reader);
+
 			} catch (IOException e) {
 				throw new UncheckedIOException("Could not read properties from " + url, e);
 			}
 		}
-		
+
 		return properties;
 	}
 }
