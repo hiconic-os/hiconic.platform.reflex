c1ab60259b9b3c33992e4fc3ceb2c0047ba2b811
Dirk Scheffler <dirk.scheffler@modularmind.eu>
2024-03-11 09:17:50 +0100

    security configuration

--- security-configuration-model/src/hiconic/rx/security/model/configuration/UsersConfiguration.java
+++ security-configuration-model/src/hiconic/rx/security/model/configuration/UsersConfiguration.java
@@ -6,11 +6,19 @@ import java.util.List;
 import com.braintribe.model.generic.GenericEntity;
 import com.braintribe.model.generic.reflection.EntityType;
 import com.braintribe.model.generic.reflection.EntityTypes;
+import com.braintribe.model.user.Group;
+import com.braintribe.model.user.Role;
 import com.braintribe.model.user.User;
 
 public interface UsersConfiguration extends GenericEntity {
 
 	EntityType<UsersConfiguration> T = EntityTypes.T(UsersConfiguration.class);
+
+	List<Role> getRoles();
+	void setRoles(List<Role> roles);
+
+	List<Group> getGroups();
+	void setGroups(List<Group> groups);
 
 	List<User> getUsers();
 	void setUsers(List<User> users);
