e23e00161b56ac43d2c891e52904da75e4ab824c
Peter Gazdik <peter.gazdik@gmail.com>
2024-05-27 19:59:26 +0200

    cleanup

--- web-rpc-client-rx-module/src/hiconic/rx/web/rpc/client/wire/WebRpcClientRxModule.java
+++ web-rpc-client-rx-module/src/hiconic/rx/web/rpc/client/wire/WebRpcClientRxModule.java
@@ -2,7 +2,6 @@ package hiconic.rx.web.rpc.client.wire;
 
 import hiconic.rx.module.api.wire.RxModule;
 
-import hiconic.rx.module.api.wire.RxModuleContract;
 import hiconic.rx.web.rpc.client.wire.space.WebRpcClientRxModuleSpace;
 
 public enum WebRpcClientRxModule implements RxModule<WebRpcClientRxModuleSpace> {
